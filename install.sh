0f3ecaaf86647e8ee24fa14f08ede0cea210f84c
Danil Utkin <utkin@majordomo.ru>
2017-12-06 14:42:48 +0300

    deleting wp-config.php before installation

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 #ENVIRONMENT has been planted
 #COPY DIST to WORKDIR
 cp -r /cms/* .;
+rm -f wp-config.php
 #CONFIG and INSTALL
 wp core config --locale=ru_RU --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST ;
 wp core install --path=/workdir --url=$DOMAIN_NAME --title="$APP_TITLE" --admin_user=$ADMIN_USERNAME --admin_password=$ADMIN_PASSWORD --skip-email --admin_email=$ADMIN_EMAIL;
