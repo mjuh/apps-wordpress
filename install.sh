fad3effe22be6fdeb505540f79e5ffa2d00bac78
Danil Utkin <utkin@majordomo.ru>
2017-12-20 17:42:16 +0300

    read db_prefix from urandom

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 cp -r /cms/* .;
 rm -f wp-config.php
 #CONFIG and INSTALL
-wp core config --locale=ru_RU --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST ;
+DB_PREFIX=$(cat /dev/urandom | tr -dc a-z | head -c 4)_
+wp core config --locale=ru_RU --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --dbprefix=$DB_PREFIX;
 wp core install --path=/workdir --url=$DOMAIN_NAME --title="$APP_TITLE" --admin_user=$ADMIN_USERNAME --admin_password=$ADMIN_PASSWORD --skip-email --admin_email=$ADMIN_EMAIL;
 
