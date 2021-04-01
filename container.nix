c90271a3e9511e36983c0f9e4738b16b511b9c07
Alexander Rezvov <rezvov@majordomo.ru>
2021-04-01 13:45:30 +0300

    Autocreate index

--- container.nix
+++ container.nix
@@ -45,6 +45,8 @@ let
 
       ${wp-cli}/bin/wp language core install ru_RU
       ${wp-cli}/bin/wp language core activate ru_RU
+
+      ${mariadb.client}/bin/mysql -h$DB_HOST -u$DB_USER -p$DB_PASSWORD $DB_NAME -e "CREATE INDEX autoload ON wp_options(autoload)"
       EOF
 
       chmod 555 $out/bin/${name}.sh
