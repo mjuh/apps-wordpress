6479956494e70adddf046fe439c78d383e9490b0
Зелинский Александр <zelinskiy@majordomo.ru>
2022-06-14 12:00:28 +0300

    bump version 6.0

--- pkgs/wordpress-russian/default.nix
+++ pkgs/wordpress-russian/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "wordpress-russian";
-  version = "5.9.3";
+  version = "6.0";
 
   src = fetchurl {
     url = "https://ru.wordpress.org/wordpress-${version}-ru_RU.tar.gz";
-    sha256 = "sha256-qoKuQnR99xBmNof2N0amvOaOo7QricqC8L2kANBiA50=";
+    sha256 = "sha256-yN8F7vxdZuAgnYRT2WRufxigKpCrttjLY8ovyDA1IYw=";
   };
 
   sourceRoot = ".";
